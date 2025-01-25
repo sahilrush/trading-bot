6f000e7ee5c44df690ab8daf9f7ce7ac84f92138
sahilrush <sahilrevankar20co21@gmail.com>
2025-01-25 11:28:18 +0530

    removing env

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,17 @@
-
+import { getTweets } from "./get-tweets";
 
 async function main(userName: string) {
-  const newTweets: Tweet[] = await getTweets(userid)
-      for(tweet of newTweets) {
-        const tokenAddress = await getTokenFromLLM(tweet.contents)
-          if(tokenAddress) {
-            const txn = await createSwapInstruction();
-            for(let i = 0; i<SPAM_COUNT; i++){
-              sendTxn(txn)
-            }
-          }
-      } 
+  const newTweets: Tweet[] = await getTweets(userName);
+  console.log(newTweets)
+  // for (tweet of newTweets) {
+  //   const tokenAddress = await getTokenFromLLM(tweet.contents);
+  //   if (tokenAddress) {
+  //     const txn = await createSwapInstruction();
+  //     for (let i = 0; i < SPAM_COUNT; i++) {
+  //       sendTxn(txn);
+  //     }
+  //   }
+  // }
 }
 
-main()
+main("");
